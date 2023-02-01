556164a13d73cbb07fb8a8b0e6fd3cca96f8bf1c
Diarra Ibrahim <ibramulaire@pop-os.localdomain>
2023-02-01 20:54:27 +0100

    maj

--- tp/starterKitTriangleWebGL/triangle.js
+++ tp/starterKitTriangleWebGL/triangle.js
@@ -2,17 +2,14 @@
 
 var vertexShaderSource = `#version 300 es
 
-// an attribute is an input (in) to a vertex shader.
-// It will receive data from a buffer
 in vec4 Position;
-uniform mat4 MVP;//recuperation de la matrice mvp
+uniform mat4 MVP;
 uniform mat4 MODEL;
 out vec4 fragcolor;
-// all shaders have a main function
-void main() {
 
-  // gl_Position is a special variable a vertex shader
-  // is responsible for setting
+void main() 
+{
+
   fragcolor=Position;
   gl_Position= MVP* Position; 
 
@@ -21,20 +18,19 @@ void main() {
 
 var fragmentShaderSource = `#version 300 es
 
-// fragment shaders don't have a default precision so we need
-// to pick one. highp is a good default. It means "high precision"
 precision highp float;
-
-// we need to declare an output for the fragment shader
 out vec4 outColor;
 in vec4  fragcolor;
-void main() {
-  // Just set the output to a constant redish-purple
+
+void main() 
+{
+ 
   outColor = fragcolor;
 }
 `;
 
-function createShader(gl, type, source) {
+function createShader(gl, type, source) 
+{
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
@@ -48,7 +44,8 @@ function createShader(gl, type, source) {
   return undefined;
 }
 
-function createProgram(gl, vertexShader, fragmentShader) {
+function createProgram(gl, vertexShader, fragmentShader) 
+{
   var program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
@@ -70,18 +67,14 @@ function initWEBGL(gl)
   gl.enable(gl.CULL_FACE);
   gl.enable(gl.DEPTH_TEST);
 
-
   var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-  var program = createProgram(gl, vertexShader, fragmentShader);
+  //var program = createProgram(gl, vertexShader, fragmentShader);
+  var program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
 
-
-return program;
-
+  return program;
 
 }
-
-
 
 
 
@@ -90,15 +83,43 @@ function toRadian(x)
   return x*Math.PI /180;
 }
 
+
+var canvas = document.querySelector("#glcanvas");
+
+
 var positions = [
-  0.0, 0.0, 0.0,
-  1.0, 0.0, 0.0,
-  0.0, 1.0, 0.0,
-  1.0, 1.0, 0.0
+  -0.5, -0.5, 0.5,
+  0.5, -0.5, 0.5,
+  -0.5, 0.5, 0.5,
+  0.5, 0.5, 0.5,
+
+  -0.5, -0.5, -0.5,
+  0.5, -0.5, -0.5,
+  -0.5, 0.5, -0.5,
+  0.5, 0.5, -0.5
+ 
 ];
 var indices = [
-  0, 3, 2
-     
+  
+  0, 3, 2,
+  0, 1, 3, 
+
+  1,7,3,
+  1,5,7,
+  
+
+  5,6,7,
+  5,4,6,
+  
+  4,2,6,
+  4,0,2,
+
+  2,7,6,
+  2,3,7,
+
+  4,1,0,
+  4,5,1,
+  
 ];
 
 var vao;
@@ -107,9 +128,9 @@ var vbo ;
 
 var cameraPosition=[0.,0.,3.];
 var cameraDistance=0.;
-var mouseLeftDown;
-var mouseRightDown;
-var mouseMiddleDown;
+var mouseLeftDown=false;
+var mouseRightDown=false;
+var mouseMiddleDown=false;
 var mouseX=0.0;
 var mouseY=0.0;
 var cameraAngleX=0.;
@@ -119,124 +140,152 @@ var cameraAngleY=0.;
 
 
 
-function genereBuffer(gl,indexVertex) {
+function genereBuffer(gl,indexVertex) 
+{
 
-  // Créer un tampon des coordonnées des sommets pour le triangle.
+  ///////VBO///////
   vbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-  gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(positions),
-                gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(positions),gl.STATIC_DRAW);
 
-
+  ///////IBO///////
   ibo = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-    new Int32Array(indices),
-    gl.STATIC_DRAW);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Int32Array(indices),gl.STATIC_DRAW);
 
+
+  ///////VAO///////
   vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
     gl.enableVertexAttribArray(indexVertex);
-
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.vertexAttribPointer(indexVertex,3,gl.FLOAT,false,0,0); 
-  // démarrer à partir de combien d'octets dans le tampon
-  
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, null);
-gl.bindVertexArray(null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindVertexArray(null);
 
- 
 }
 
 function deleteVBO (gl)
 //-----------------
 {
   gl.deleteBuffer(vbo);
-  gl.deleteBuffer(ibo);
- 
-    
+  gl.deleteBuffer(ibo);   
 }
 
-function drawScene(gl, programInfo) {
-  gl.clearColor(1.0,1.0,1.0,0.0);  // effacement en noir, complètement opaque
-  gl.clearDepth(1.0);                 // tout effacer
-
-  // Effacer le canevas avant que nous ne commencions à dessiner dessus.
+function drawScene(gl, programInfo) 
+{
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0.0,0.0,0.0,0.0); 
+  gl.clearDepth(10.0);                 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
 
-
-
-  // Créer une matrice de perspective
-  // Champ de vision de 45 degrés, avec un rapport largeur/hauteur qui correspond à la taille d'affichage du canvas, profondeur entre 0,1 et 100
+  //alert(gl.canvas.clientWidth);
   var fieldOfView = 60 * Math.PI / 180;   // en radians
-  var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  var aspect = canvas.clientWidth / canvas.clientHeight;
   var zNear = 0.1;
   var zFar = 1000.0;
-  var Projection = mat4.create();
-
-  // note: glmatrix.js a toujours comme premier argument la destination où stocker le résultat.  
+  var Projection = mat4.create();  
   mat4.perspective(Projection,
                    fieldOfView,
                    aspect,
                    zNear,
                    zFar);
 
-  // Définir la position de dessin comme étant le point "origine", qui est le centre de la scène.
+ 
+
   var View = mat4.create();
-
   mat4.lookAt(View,cameraPosition,[0,0,0],[0,1,0]);
-  // translation
+  
+
+
   var Model = mat4.create();
-
-
-  mat4.translate(Model,  Model, [0,0,cameraDistance]);  // amount to translate
-  mat4.rotate(Model,  Model,toRadian(cameraAngleX), [1,0,0]); 
-  mat4.rotate(Model, Model,toRadian(cameraAngleY) , [0,1,0]); 
+  mat4.translate(Model,Model,[0,0,cameraDistance]);  
+  mat4.rotate(Model,Model,toRadian(cameraAngleX),[1,0,0]); 
+  mat4.rotate(Model,Model,toRadian(cameraAngleY),[0,1,0]); 
+  mat4.scale(Model,Model,[0.8,0.8,0.8]);
  
+
+
   var MVP= mat4.create();
- mat4.multiply(MVP,Projection,View);
- mat4.multiply(MVP,MVP,Model);
- 
+  mat4.multiply(MVP,Projection,View);
+  mat4.multiply(MVP,MVP,Model);
+  
 
- 
-// Indiquer à WebGL d'utiliser notre programme pour dessiner
-gl.useProgram(programInfo.programID);
+  gl.useProgram(programInfo.programID);
+  gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDMVP,false, (MVP));
+  gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDView,false,(View));
+  gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDModel,false,(Model));
+  gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDPerspective,false, (Projection));
 
-// Définir les uniformes du shader
-gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDMVP,false, (MVP));
-gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDView,false,(View));
-gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDModel,false,(Model));
-gl.uniformMatrix4fv(programInfo.uniformLocations.MatrixIDPerspective,false, (Projection));
+  gl.uniform3f( programInfo.uniformLocations.locCameraPosition ,cameraPosition.x, cameraPosition.y, cameraPosition.z);
 
-gl.uniform3f( programInfo.uniformLocations.locCameraPosition ,cameraPosition.x, cameraPosition.y, cameraPosition.z);
-
-gl.bindVertexArray(vao);
-
+  gl.bindVertexArray(vao);
 
 
    gl.drawElements(gl.TRIANGLES,indices.length , gl.UNSIGNED_INT, 0);
   
 }
 
+function updateMousePosition(e){
+  var x = e.offsetX;
+  var y = e.offsetY;
+  if(mouseLeftDown)
+    {
+      cameraAngleY+=(x-mouseX);
+      cameraAngleX+=(y-mouseY);
+      mouseX=x;
+      mouseY=y;
+    }
+    if(mouseMiddleDown)
+    {
+        cameraDistance += (y - mouseY) * 0.2;
+        mouseY = y;
+    }
+ 
+  
+}
 
+function mouseDown(e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  mouseX=x;
+  mouseY=y;
+  if(e.button==0)
+  mouseLeftDown=true;
+  else
+  if(e.button==1)
+  mouseMiddleDown=true;
+  else
+  if(e.button==2)
+  mouseRightDown=true;
+ 
+}
 
-
-
-
-
-
-
-
-
-
+function mouseUp(e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  mouseX=x;
+  mouseY=y;
+  if(e.button==0)
+  mouseLeftDown=false;
+  else
+  if(e.button==1)
+  mouseMiddleDown=false;
+  else
+  if(e.button==2)
+  mouseRightDown=false;
+}
 
 function main() {
   // Get A WebGL context
-  var canvas = document.querySelector("#glcanvas");
   var gl = canvas.getContext("webgl2");
+  canvas.addEventListener( 'mousedown', mouseDown, false );
+  canvas.addEventListener( 'mouseup', mouseUp, false );
+  canvas.addEventListener( 'mousemove', updateMousePosition, false );
   if (!gl) {
     return;
   }
@@ -273,13 +322,13 @@ function main() {
     };
     genereBuffer(gl,programInfo.attribLocations.vertexPosition);
     function render() {
-
+         
           drawScene(gl, programInfo);
           requestAnimationFrame(render);
     }
   requestAnimationFrame(render);
 
-  //deleteVBO (gl);
+
 }
 
 
