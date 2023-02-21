b4be76b3d52c27b26830494024b2e6b7fa90bcf7
ibramulaire <ibramulaire@gmail.com>
2023-02-21 12:14:43 +0100

    maj

--- DIarra IBrahim/triangle.js
+++ DIarra IBrahim/triangle.js
@@ -486,9 +486,9 @@ var n=20
     0,0,0,
     -0.5, -0.5, 0,
     0.5, -0.5, 0,
-    0.8, 0.5, 0,
-    0,1,0,
-    -0.8, 0.5, 0,
+    0.8, 0.25, 0,
+    0,0.8,0,
+    -0.8, 0.25, 0,
    
   ];
   indices = [
@@ -503,6 +503,30 @@ var n=20
   vaopentagone=  genereBuffer(gl,vertexPosition);
   ibopentagone=indices.length;
 
+
+
+  positions = [
+    0,0,0,
+    -0.5, -0.8, 0,
+    0.5, -0.8, 0,
+    0.8, 0, 0,
+    0.5, 0.8, 0,
+    -0.5, 0.8, 0,
+    
+    -0.8, 0, 0,
+   
+  ];
+  indices = [
+    0,1,2,
+    0,2,3,
+    0,3,4,
+    0,4,5,
+    0,5, 6,
+    0,6, 1
+  
+  ];
+  vaohexagone=  genereBuffer(gl,vertexPosition);
+  ibohexagone=indices.length;
 
 }
 function main() {
